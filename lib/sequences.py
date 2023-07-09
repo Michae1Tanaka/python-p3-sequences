da8cc87d0ef2dbacc9f3dc8b1f8c340220796280
Michael Tanaka <tanakam4214@gmail.com>
2023-07-09 15:21:35 -0700

    Lab Completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+import ipdb
+
 
 def print_fibonacci(length):
-    pass
+    fib1 = 0
+    fib2 = 1
+    fib_list = []
+    if length <= 0:
+        print(fib_list)
+    elif length == 1:
+        fib_list = [fib1]
+        print(fib_list)
+    elif length == 2:
+        fib_list = [fib1, fib2]
+        print(fib_list)
+    else:
+        fib_list = [fib1, fib2]
+        while len(fib_list) < length:
+            fib2 = fib_list[-2]
+            fib1 = fib_list[-1]
+            fib3 = fib1 + fib2
+            fib_list.append(fib3)
+        print(fib_list)
